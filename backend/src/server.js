281c4c96d5bc709e5093d8a41bcc38647addeed2
jeslin-star-14 <justinpadma31@gmail.com>
2026-03-11 22:21:52 +0530

    jeslin

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,31 +1,9 @@
-import http from "http"
-import { Server } from "socket.io"
-import dotenv from "dotenv"
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/database.js";
 
-import connectDB from "./config/database.js"
-import createApp from "./app.js"
-import { initSocket } from "./sockets/socketManager.js"
+dotenv.config();
 
-dotenv.config()
+connectDB();
 
-connectDB()
-
-const PORT = process.env.PORT || 5000
-
-const httpServer = http.createServer()
-
-const io = new Server(httpServer, {
-  cors: {
-    origin: "*"
-  }
-})
-
-const app = createApp(io)
-
-httpServer.on("request", app)
-
-initSocket(io)
-
-httpServer.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+const app = express();
